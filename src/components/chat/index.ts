6ec739a928ddc602f87e893d8c6a2b1c07f98107
VarunTej <varuntejreddy03@gmail.com>
2026-07-10 12:23:29 +0530

    feat: add chat barrel export (#2)

---
+++ src/components/chat/index.ts
@@ -0,0 +1,8 @@
+export { ChatHeader } from './ChatHeader';
+export { MessageBubble } from './MessageBubble';
+export { AssistantMessage } from './AssistantMessage';
+export { TypingIndicator } from './TypingIndicator';
+export { ChatInput } from './ChatInput';
+export { QuickActions } from './QuickActions';
+export { AssessmentCard } from './AssessmentCard';
+export { PermissionCard, MCQCard, TextQuestionCard, SummaryCard } from './DiagnosticCard';
